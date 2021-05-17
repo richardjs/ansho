2a4233634cc97b9d951fa396d3ac807b0032e161
Richard Schneider <richard@schneiderbox.net>
2021-05-16 20:44:21 -0500

    Implement AnshoText.load

--- src/lexer.js
+++ src/lexer.js
@@ -5,39 +5,51 @@ const TOKEN_DELIMITERS = SEGMENT_DELIMITERS + ' ';
 // now the same characters are checked multiple times.
 
 export class Token {
-    constructor(text) {
+    constructor() {
         this.string = '';
         this.suffix = '';
+    }
+
+    static parse(text) {
+        const token = new Token();
+
         for (const c of text) {
             if (TOKEN_DELIMITERS.includes(c)) {
-                this.suffix += c;
+                token.suffix += c;
             } else {
-                this.string += c;
+                token.string += c;
             }
         }
+
+        return token;
     }
 }
 
 
 export class Segment {
-    constructor(text) {
+    constructor() {
         this.tokens = [];
+    }
         
+    static parse(text) {
+        const segment = new Segment();
+
         let tokenText = '';
         let inDelimiter = false;
         for (const c of text) {
             if (TOKEN_DELIMITERS.includes(c)) {
                 inDelimiter = true; 
             } else if (inDelimiter) {
-                this.tokens.push(new Token(tokenText));
+                segment.tokens.push(Token.parse(tokenText));
                 tokenText = '';
                 inDelimiter = false;
             }
 
             tokenText += c;
         }
+        segment.tokens.push(Token.parse(tokenText));
 
-        this.tokens.push(new Token(tokenText));
+        return segment;
     }
 }
 
@@ -56,33 +68,48 @@ export class AnshoText {
             if (SEGMENT_DELIMITERS.includes(c)) {
                inDelimiter = true;
             } else if (inDelimiter && !TOKEN_DELIMITERS.includes(c)) {
-                text.segments.push(new Segment(segmentText));
+                text.segments.push(Segment.parse(segmentText));
                 segmentText = '';
                 inDelimiter = false;
             }
             segmentText += c;
         }
+        text.segments.push(Segment.parse(segmentText));
 
-        text.segments.push(new Segment(segmentText));
         return text;
     }
 
     static load(json) {
+        const data = JSON.parse(json);
+        const text = new AnshoText();
+
+        for (const segmentData of data.segments) {
+            const segment = new Segment();
+            for (const tokenData of segmentData) {
+                const token = new Token();
+                token.string = tokenData.string;
+                token.suffix = tokenData.suffix;
+                segment.tokens.push(token);
+            }
+            text.segments.push(segment);
+        }
+
+        return text;
     }
 
     toJSON() {
-        const json = {"segments": []}
+        const data = {"segments": []}
         for (const segment of this.segments) {
-            const segmentJSON = [];
+            const segmentData = [];
             for (const token of segment.tokens) {
-                segmentJSON.push({
+                segmentData.push({
                     "string": token.string,
                     "suffix": token.suffix,
                 })
             }
-            json.segments.push(segmentJSON);
+            data.segments.push(segmentData);
         }
 
-        return JSON.stringify(json);
+        return JSON.stringify(data);
     }
 }
