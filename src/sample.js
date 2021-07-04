d8da8b7a6457e71de8a5192790d61a4c4df6def9
Richard Schneider <richard@schneiderbox.net>
2021-07-03 19:48:16 -0500

    Add a couple more sample texts

--- src/sample.js
+++ src/sample.js
@@ -30,3 +30,24 @@ God is his own interpreter,
 
 
 export const SAMPLE_2 = `It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way⁠—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.`
+
+
+// From https://en.wikipedia.org/wiki/Old_Roman_Symbol
+export const SAMPLE_3 = `Πιστεύω οὖν εἰς θεòν πατέρα παντοκράτορα·
+καὶ εἰς Χριστὸν Ἰησοῦν, τὸν υἱὸν αὐτοῦ τὸν μονογενῆ, τὸν κύριον ἡμῶν,
+τὸν γεννηθέντα ἐκ πνεύματος ἁγίου καὶ Μαρίας τῆς παρθένου,
+τὸν ἐπὶ Ποντίου Πιλάτου σταυρωθέντα καὶ ταφέντα
+καὶ τῇ τρίτῃ ἡμέρα ἀναστάντα ἐκ τῶν νεκρῶν,
+ἀναβάντα εἰς τοὺς οὐρανούς
+καὶ καθήμενον ἐν δεξιᾳ τοῦ πατρός, ὅθεν ἔρχεται κρίνειν ζῶντας καὶ νεκρούς·
+καὶ εἰς τò ἅγιον πνεῦμα,
+ἁγίαν ἐκκλησίαν,
+ἄφεσιν ἁμαρτιῶν,
+σαρκὸς ἀνάστασιν,
+ζωὴν αἰώνιον.`
+
+
+export const SAMPLE_4 = `He has shown you, O mortal, what is good.
+    And what does the Lord require of you?
+To act justly and to love mercy
+    and to walk humbly with your God.` // NIV
